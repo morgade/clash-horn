3ea8257d114373c6c5bc2308f7ba9b75999f4e2a
morgade <morgade@gmail.com>
2017-09-11 14:44:03 -0300

    Spring boot boilerplate commit

---
+++ clash-horn-service/src/main/java/com/clashhorn/model/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.clashhorn.model;
+
+/**
+ *
+ * @author x4rb
+ */
+public interface CommentRepository {
+    Iterable<Comment> findAll();
+    Comment save(Comment comment);
+    Comment get(String commentId);
+    void remove(String commentId);
+}
